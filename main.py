5882af6ba9826df77c21c84209d762cd01dc4149
Fatima Abdillahi <fatimabdillahi@gmail.com>
2025-04-29 13:11:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import asyncio
 from joke_engine import generate_joke_audio
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    task = asyncio.create_task(loop())
+    yield
+    task.cancel()  # Clean up when shutting down
+
+async def loop():
+    while True:
+        await generate_joke_audio()
+        await asyncio.sleep(90)
+
+app = FastAPI(lifespan=lifespan)  # Combine everything into one FastAPI instance
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root():
     return {"message": "DELI is roasting."}
-
-@app.on_event("startup")
-async def run_background_task():
-    async def loop():
-        while True:
-            await generate_joke_audio()
-            await asyncio.sleep(90)
-    asyncio.create_task(loop())
