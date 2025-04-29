b31332131389263676d89659bd30478e5683f921
Fatima Abdillahi <fatimabdillahi@gmail.com>
2025-04-29 13:21:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ from contextlib import asynccontextmanager
 import os
 import uvicorn
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT isn't set
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     task = asyncio.create_task(loop())
     yield
-    task.cancel()  # Clean up when shutting down
+    task.cancel()
 
 async def loop():
     while True:
         await generate_joke_audio()
         await asyncio.sleep(90)
 
-app = FastAPI(lifespan=lifespan)  # Combine everything into one FastAPI instance
+# Define FastAPI instance with lifespan events
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root():
     return {"message": "DELI is roasting."}
+
+# Ensure main execution starts correctly
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT isn't set
+    uvicorn.run(app, host="0.0.0.0", port=port)
