f954a455b30aaaa9040bf73aa0272e5764ecb2fb
Fatima Abdillahi <fatimabdillahi@gmail.com>
2025-04-29 18:01:03 -0400

    Update joke_engine.py

--- joke_engine.py
+++ joke_engine.py
@@ -1,9 +1,7 @@
 import os
 from openai import OpenAI
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
 
 # Initialize OpenAI client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
